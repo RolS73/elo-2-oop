064489d4c8bc2824af25548863dbbbe6548c34f1
RolS73 <73384621+RolS73@users.noreply.github.com>
2021-03-24 15:50:03 +0100

    Initial Solution

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,42 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
 
+    private List<Animal> farm;
+
+    public Farm(List<Animal> animals) {
+        this.farm = animals;
+    }
+
+    public void feedAnimals() {
+        for (Animal animal : this.farm) {
+            animal.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        this.farm.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return this.farm.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        List<String> statusList = new ArrayList<>();
+        for (Animal animal : this.farm) {
+            statusList.add("There is a " + animal.getSize() + " sized "
+                    + animal.getLowercaseClassSimpleName() + " in the farm.");
+        }
+        return statusList;
+    }
+
+    public List<Animal> getAnimals() {
+        return farm;
+    }
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,24 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    private int size;
+
+    public Animal() {
+        this.size = 0;
+    }
+
+    public abstract void feed();
+
+    public int getSize() {
+        return size;
+    }
+
+    protected void setSize(int sizeAddition) {
+        this.size += sizeAddition;
+    }
+
+
+    public String getLowercaseClassSimpleName() {
+        return this.getClass().getSimpleName().toLowerCase();
+    }
 }
